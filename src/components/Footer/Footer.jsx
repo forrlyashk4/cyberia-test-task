3fa308c13ba5d95aedff10bb45b4b175ec16c2f6
Nikita Rudometov <nikitarudometof@gmail.com>
2024-05-18 12:42:55 +0700

    feat: implement footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,13 +5,13 @@ import logotype from "../../assets/icons/cyberia-logo.svg";
 export default function Footer() {
   return (
     <footer>
-      <div className="container">
+      <div className={`container ${styles.footer_container}`}>
         <div>
           <img src={logotype} alt="Cyberia logotype" />
           <p>Веб-разработка и усиление IT-команд</p>
         </div>
         <div className={styles.footer_links}>
-          <a href="+79991234567">+7 999 123 45 67</a>
+          <a href="tel:+79991234567">+7 999 123 45 67</a>
           <a href="mailto:hello@cyberia.studio">hello@cyberia.studio</a>
           <span>ул.Ярных, д.35, оф.10</span>
           <a href="./">Агентство</a>
